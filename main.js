ab1c8d87d48d3c0faee64a5ea349a720e5c386a2
schaerta <tatiana@govolunteer.com>
2019-02-20 11:55:07 +0100

    Addes styles, an image and a link

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var app = new Vue({
   el: '#app',
   data: {
-    product: 'Coat',
-    description: 'Beautiful, white and soft.'
+    product: 'Socks',
+    description: 'Beautiful and soft.',
+    image: './img/socks.jpeg',
+    moreProductsLink: 'https://www.ecosia.org/',
+    MoreProductsText: 'Search for more products...'
   }
 })
