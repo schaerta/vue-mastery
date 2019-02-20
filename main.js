17ab9bbc1abe9de1c342ba07ed5c461c1e49cb94
schaerta <tatiana@govolunteer.com>
2019-02-20 12:06:51 +0100

    Added stock info

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
+    inventory: 9,
     description: 'Beautiful and soft.',
     image: './img/socks.jpeg',
     moreProductsLink: 'https://www.ecosia.org/',
