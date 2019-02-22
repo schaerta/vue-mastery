2dd86e9444e7fb4cff68bf95c6462abf9f35c795
schaerta <tatiana@govolunteer.com>
2019-02-22 20:44:46 +0100

    Added variant images and show them on hover

--- main.js
+++ main.js
@@ -4,8 +4,34 @@ var app = new Vue({
     product: 'Socks',
     inventory: 9,
     description: 'Beautiful and soft.',
-    image: './img/socks.jpeg',
+    image: './img/socks_green.jpeg',
+    details: ["80% cotton", "20% polyester"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+        variantImage: "./img/socks_green.jpeg"
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+        variantImage: "./img/socks_blue.jpeg"
+      }
+    ],
+    cart: 0,
     moreProductsLink: 'https://www.ecosia.org/',
     MoreProductsText: 'Search for more products...'
+  },
+
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    subtractFromCart() {
+      this.cart -= 1;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
+    }
   }
 })
