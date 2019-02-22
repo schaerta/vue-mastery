cd7d7a8db356a3f85df301dd0dfcbf93246a17e2
schaerta <tatiana@govolunteer.com>
2019-02-22 21:43:43 +0100

    Variant Refact

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 var app = new Vue({
   el: '#app',
   data: {
+    brand: 'Vue Mastery',
     product: 'Socks',
     inventory: 9,
     description: 'Beautiful and soft.',
-    image: './img/socks_green.jpeg',
+    selectedVariant: 0,
     details: ["80% cotton", "20% polyester"],
     variants: [
       {
         variantId: 2234,
         variantColor: 'green',
-        variantImage: "./img/socks_green.jpeg"
+        variantImage: "./img/socks_green.jpeg",
+        variantQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: 'blue',
-        variantImage: "./img/socks_blue.jpeg"
+        variantImage: "./img/socks_blue.jpeg",
+        variantQuantity: 0
       }
     ],
     cart: 0,
@@ -30,8 +33,20 @@ var app = new Vue({
     subtractFromCart() {
       this.cart -= 1;
     },
-    updateProduct(variantImage) {
-      this.image = variantImage
+    updateProduct(index) {
+      this.selectedVariant = index;
+    }
+  },
+
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity
     }
   }
 })
